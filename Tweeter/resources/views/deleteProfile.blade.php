a45be657f2d3a4129ea9b38c7d04636b55577ce2
mullersen <mariannhaugland@gmail.com>
2020-02-19 04:15:22 +0000

    fixed get bug

--- Tweeter/resources/views/deleteProfile.blade.php
+++ Tweeter/resources/views/deleteProfile.blade.php
@@ -12,7 +12,7 @@
                 <button class="form-control btn btn-outline-danger" type='submit' name='id' value='{{Auth::user()->id}}'>Yes</button>
             </div>
         </form>
-        <form action='/profile/show/{{{Auth::user()->id}}}' method='post'>
+        <form action='/profile/show/{{{Auth::user()->id}}}' method='get'>
             @csrf
             <div class="form-group">
                 <button class="form-control bg-light" type='submit'name='id' value='{{Auth::user()->id}}'>No, take me back</button>
